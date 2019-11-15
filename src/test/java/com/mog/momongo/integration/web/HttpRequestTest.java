390a9b93f6ecc4e21e18fcc673c6938672b25b5b
ram <meerdara@hotmail.com>
2019-11-15 17:00:15 -0500

    added web layer tests

---
+++ src/test/java/com/mog/momongo/integration/web/HttpRequestTest.java
@@ -0,0 +1,32 @@
+package com.mog.momongo.integration.web;
+
+import com.mog.momongo.BaseTest;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+/**
+ * HttpServerPortTest
+ */
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest extends BaseTest{
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello World");
+    }
+}
